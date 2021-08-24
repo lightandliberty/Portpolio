65ededd2e3d3b3400d1c03f7d5e834fa83579eef
이기석 <ssamjangcool@gmail.com>
2021-08-24 17:32:15 +0900

    complete add ControlBox Button

--- BasicProperties_dll/Properties.Designer.cs
+++ BasicProperties_dll/Properties.Designer.cs
@@ -28,25 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.TopMostBtn = new System.Windows.Forms.Button();
+            this.SetNotTopMostBtn = new System.Windows.Forms.Button();
+            this.ReadFromAppConfigBtn = new System.Windows.Forms.Button();
+            this.LoadBitmapBtn = new System.Windows.Forms.Button();
+            this.ChangeFormBorderStyleBtn = new System.Windows.Forms.Button();
+            this.SetControlBoxBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // TopMostBtn
             // 
-            this.button1.Location = new System.Drawing.Point(40, 38);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "최상위창";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.TopMostBtn_Click);
+            this.TopMostBtn.Location = new System.Drawing.Point(40, 38);
+            this.TopMostBtn.Name = "TopMostBtn";
+            this.TopMostBtn.Size = new System.Drawing.Size(75, 23);
+            this.TopMostBtn.TabIndex = 0;
+            this.TopMostBtn.Text = "최상위창";
+            this.TopMostBtn.UseVisualStyleBackColor = true;
+            this.TopMostBtn.Click += new System.EventHandler(this.TopMostBtn_Click);
+            // 
+            // SetNotTopMostBtn
+            // 
+            this.SetNotTopMostBtn.Location = new System.Drawing.Point(121, 38);
+            this.SetNotTopMostBtn.Name = "SetNotTopMostBtn";
+            this.SetNotTopMostBtn.Size = new System.Drawing.Size(91, 23);
+            this.SetNotTopMostBtn.TabIndex = 1;
+            this.SetNotTopMostBtn.Text = "최상위 아님";
+            this.SetNotTopMostBtn.UseVisualStyleBackColor = true;
+            this.SetNotTopMostBtn.Click += new System.EventHandler(this.SetNotTopMostBtn_Click);
+            // 
+            // ReadFromAppConfigBtn
+            // 
+            this.ReadFromAppConfigBtn.Location = new System.Drawing.Point(40, 9);
+            this.ReadFromAppConfigBtn.Name = "ReadFromAppConfigBtn";
+            this.ReadFromAppConfigBtn.Size = new System.Drawing.Size(172, 23);
+            this.ReadFromAppConfigBtn.TabIndex = 2;
+            this.ReadFromAppConfigBtn.Text = "App.config에서 값 읽어오기";
+            this.ReadFromAppConfigBtn.UseVisualStyleBackColor = true;
+            this.ReadFromAppConfigBtn.Click += new System.EventHandler(this.ReadFromAppConfigBtn_Click);
+            // 
+            // LoadBitmapBtn
+            // 
+            this.LoadBitmapBtn.Location = new System.Drawing.Point(40, 67);
+            this.LoadBitmapBtn.Name = "LoadBitmapBtn";
+            this.LoadBitmapBtn.Size = new System.Drawing.Size(178, 23);
+            this.LoadBitmapBtn.TabIndex = 3;
+            this.LoadBitmapBtn.Text = "c:\\cocoa.png 파일 나타내기";
+            this.LoadBitmapBtn.UseVisualStyleBackColor = true;
+            this.LoadBitmapBtn.Click += new System.EventHandler(this.LoadBitmapBtn_Click);
+            // 
+            // ChangeFormBorderStyleBtn
+            // 
+            this.ChangeFormBorderStyleBtn.Location = new System.Drawing.Point(40, 96);
+            this.ChangeFormBorderStyleBtn.Name = "ChangeFormBorderStyleBtn";
+            this.ChangeFormBorderStyleBtn.Size = new System.Drawing.Size(172, 23);
+            this.ChangeFormBorderStyleBtn.TabIndex = 4;
+            this.ChangeFormBorderStyleBtn.Text = "FormBorderStyle속성 설정";
+            this.ChangeFormBorderStyleBtn.UseVisualStyleBackColor = true;
+            this.ChangeFormBorderStyleBtn.Click += new System.EventHandler(this.ChangeFormBorderStyleBtn_Click);
+            // 
+            // SetControlBoxBtn
+            // 
+            this.SetControlBoxBtn.Location = new System.Drawing.Point(40, 125);
+            this.SetControlBoxBtn.Name = "SetControlBoxBtn";
+            this.SetControlBoxBtn.Size = new System.Drawing.Size(172, 23);
+            this.SetControlBoxBtn.TabIndex = 5;
+            this.SetControlBoxBtn.Text = "ControlBox 속성 변경";
+            this.SetControlBoxBtn.UseVisualStyleBackColor = true;
+            this.SetControlBoxBtn.Click += new System.EventHandler(this.SetControlBoxBtn_Click);
             // 
             // Properties
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SetControlBoxBtn);
+            this.Controls.Add(this.ChangeFormBorderStyleBtn);
+            this.Controls.Add(this.LoadBitmapBtn);
+            this.Controls.Add(this.ReadFromAppConfigBtn);
+            this.Controls.Add(this.SetNotTopMostBtn);
+            this.Controls.Add(this.TopMostBtn);
             this.Name = "Properties";
             this.ResumeLayout(false);
 
@@ -54,6 +114,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button TopMostBtn;
+        private System.Windows.Forms.Button SetNotTopMostBtn;
+        private System.Windows.Forms.Button ReadFromAppConfigBtn;
+        private System.Windows.Forms.Button LoadBitmapBtn;
+        private System.Windows.Forms.Button ChangeFormBorderStyleBtn;
+        private System.Windows.Forms.Button SetControlBoxBtn;
     }
 }
--- BasicProperties_dll/Properties.cs
+++ BasicProperties_dll/Properties.cs
@@ -12,14 +12,79 @@ namespace BasicProperties_dll
 {
     public partial class Properties: Form
     {
+        Color mControlColor;
         public Properties()
         {
             InitializeComponent();
+            mControlColor = this.BackColor;
         }
 
         private void TopMostBtn_Click(object sender, EventArgs e)
         {
             this.TopMost = true;
+        }
+
+        private void SetNotTopMostBtn_Click(object sender, EventArgs e)
+        {
+            this.TopMost = false;
+        }
+
+        private void ReadFromAppConfigBtn_Click(object sender, EventArgs e)
+        {
+            System.Configuration.AppSettingsReader appSettings = new System.Configuration.AppSettingsReader();
+            try
+            {
+                object o = appSettings.GetValue("MainFormOpacity", typeof(double));
+                if(o != null)
+                    MessageBox.Show(((double)o).ToString());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("각 프로젝트의 App.config파일 안에 <configuration>안에 <appSettings> </appSettings>사이에 <add key = \"MainFormOpacity\" value=\".5\" /> 를 추가해 주세요.");
+            }
+        }
+
+        private void LoadBitmapBtn_Click(object sender, EventArgs e)
+        {
+            try { Bitmap backgroundImage = new Bitmap(@"C:\cocoa.png"); }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString() + @" C:\cocoa.png 파일이 없습니다.");
+                MessageBox.Show("1. 추가_기존항목으로 추가. 2.파일 오른쪽 클릭_속성_빌드작업_포함 리소스 선택 후, new Bitmap(this.GetType(), \"cocoa.png\" 하면 됩니다. 그러면, 리소스 이름은 Properties.cocoa.png이 된다.");
+                MessageBox.Show("아니면 디자이너에서 [속성]_BackgroundImage옆 ... 을 누른뒤에 선택한다.");
+            }
+        }
+
+        private void ChangeFormBorderStyleBtn_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = 
+                this.FormBorderStyle.Equals(FormBorderStyle.SizableToolWindow) ? FormBorderStyle.FixedToolWindow :
+                this.FormBorderStyle.Equals(FormBorderStyle.FixedToolWindow) ? FormBorderStyle.FixedDialog :
+                this.FormBorderStyle.Equals(FormBorderStyle.FixedDialog) ? FormBorderStyle.FixedSingle : 
+                this.FormBorderStyle.Equals(FormBorderStyle.Sizable) ? FormBorderStyle.SizableToolWindow :
+                FormBorderStyle.Sizable;
+            if (!this.FormBorderStyle.Equals(FormBorderStyle.Sizable))
+            {
+                if(FormBorderStyle.Equals(FormBorderStyle.SizableToolWindow))
+                    ChangeFormBorderStyleBtn.BackColor = Color.Red;
+                else
+                    ChangeFormBorderStyleBtn.BackColor = System.Drawing.Color.FromArgb(255, ChangeFormBorderStyleBtn.BackColor.R-15, ChangeFormBorderStyleBtn.BackColor.G + 40, ChangeFormBorderStyleBtn.BackColor.B + 40);
+            }
+            else
+                ChangeFormBorderStyleBtn.BackColor = Color.Silver;
+            this.Text = this.FormBorderStyle.ToString();
+        }
+
+        private void SetControlBoxBtn_Click(object sender, EventArgs e)
+        {
+            this.ControlBox = this.ControlBox.Equals(true) ? false : true;
+            if (this.ControlBox.Equals(false))
+                this.SetControlBoxBtn.BackColor = Color.Red;
+            else
+                this.SetControlBoxBtn.BackColor = mControlColor;
+
+            this.Text = this.ControlBox.ToString();
         }
     }
 }
