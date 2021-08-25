ed79aa4e718200d252e1f1db99d7d76b267eafda
이기석 <ssamjangcool@gmail.com>
2021-08-25 11:43:39 +0900

    Btn 설명 instrumentTextBox까지 완료

--- BasicProperties_dll/Properties.Designer.cs
+++ BasicProperties_dll/Properties.Designer.cs
@@ -29,38 +29,29 @@
         private void InitializeComponent()
         {
             this.TopMostBtn = new System.Windows.Forms.Button();
-            this.SetNotTopMostBtn = new System.Windows.Forms.Button();
             this.ReadFromAppConfigBtn = new System.Windows.Forms.Button();
             this.LoadBitmapBtn = new System.Windows.Forms.Button();
             this.ChangeFormBorderStyleBtn = new System.Windows.Forms.Button();
             this.SetControlBoxBtn = new System.Windows.Forms.Button();
+            this.SetMaxiMiniBtn = new System.Windows.Forms.Button();
+            this.instructionTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // TopMostBtn
             // 
-            this.TopMostBtn.Location = new System.Drawing.Point(40, 38);
+            this.TopMostBtn.Location = new System.Drawing.Point(40, 105);
             this.TopMostBtn.Name = "TopMostBtn";
-            this.TopMostBtn.Size = new System.Drawing.Size(75, 23);
+            this.TopMostBtn.Size = new System.Drawing.Size(200, 23);
             this.TopMostBtn.TabIndex = 0;
             this.TopMostBtn.Text = "최상위창";
             this.TopMostBtn.UseVisualStyleBackColor = true;
             this.TopMostBtn.Click += new System.EventHandler(this.TopMostBtn_Click);
             // 
-            // SetNotTopMostBtn
-            // 
-            this.SetNotTopMostBtn.Location = new System.Drawing.Point(121, 38);
-            this.SetNotTopMostBtn.Name = "SetNotTopMostBtn";
-            this.SetNotTopMostBtn.Size = new System.Drawing.Size(91, 23);
-            this.SetNotTopMostBtn.TabIndex = 1;
-            this.SetNotTopMostBtn.Text = "최상위 아님";
-            this.SetNotTopMostBtn.UseVisualStyleBackColor = true;
-            this.SetNotTopMostBtn.Click += new System.EventHandler(this.SetNotTopMostBtn_Click);
-            // 
             // ReadFromAppConfigBtn
             // 
-            this.ReadFromAppConfigBtn.Location = new System.Drawing.Point(40, 9);
+            this.ReadFromAppConfigBtn.Location = new System.Drawing.Point(40, 76);
             this.ReadFromAppConfigBtn.Name = "ReadFromAppConfigBtn";
-            this.ReadFromAppConfigBtn.Size = new System.Drawing.Size(172, 23);
+            this.ReadFromAppConfigBtn.Size = new System.Drawing.Size(200, 23);
             this.ReadFromAppConfigBtn.TabIndex = 2;
             this.ReadFromAppConfigBtn.Text = "App.config에서 값 읽어오기";
             this.ReadFromAppConfigBtn.UseVisualStyleBackColor = true;
@@ -68,9 +59,9 @@
             // 
             // LoadBitmapBtn
             // 
-            this.LoadBitmapBtn.Location = new System.Drawing.Point(40, 67);
+            this.LoadBitmapBtn.Location = new System.Drawing.Point(40, 134);
             this.LoadBitmapBtn.Name = "LoadBitmapBtn";
-            this.LoadBitmapBtn.Size = new System.Drawing.Size(178, 23);
+            this.LoadBitmapBtn.Size = new System.Drawing.Size(200, 23);
             this.LoadBitmapBtn.TabIndex = 3;
             this.LoadBitmapBtn.Text = "c:\\cocoa.png 파일 나타내기";
             this.LoadBitmapBtn.UseVisualStyleBackColor = true;
@@ -78,9 +69,9 @@
             // 
             // ChangeFormBorderStyleBtn
             // 
-            this.ChangeFormBorderStyleBtn.Location = new System.Drawing.Point(40, 96);
+            this.ChangeFormBorderStyleBtn.Location = new System.Drawing.Point(40, 163);
             this.ChangeFormBorderStyleBtn.Name = "ChangeFormBorderStyleBtn";
-            this.ChangeFormBorderStyleBtn.Size = new System.Drawing.Size(172, 23);
+            this.ChangeFormBorderStyleBtn.Size = new System.Drawing.Size(200, 23);
             this.ChangeFormBorderStyleBtn.TabIndex = 4;
             this.ChangeFormBorderStyleBtn.Text = "FormBorderStyle속성 설정";
             this.ChangeFormBorderStyleBtn.UseVisualStyleBackColor = true;
@@ -88,37 +79,58 @@
             // 
             // SetControlBoxBtn
             // 
-            this.SetControlBoxBtn.Location = new System.Drawing.Point(40, 125);
+            this.SetControlBoxBtn.Location = new System.Drawing.Point(40, 192);
             this.SetControlBoxBtn.Name = "SetControlBoxBtn";
-            this.SetControlBoxBtn.Size = new System.Drawing.Size(172, 23);
+            this.SetControlBoxBtn.Size = new System.Drawing.Size(200, 23);
             this.SetControlBoxBtn.TabIndex = 5;
             this.SetControlBoxBtn.Text = "ControlBox 속성 변경";
             this.SetControlBoxBtn.UseVisualStyleBackColor = true;
             this.SetControlBoxBtn.Click += new System.EventHandler(this.SetControlBoxBtn_Click);
+            // 
+            // SetMaxiMiniBtn
+            // 
+            this.SetMaxiMiniBtn.Location = new System.Drawing.Point(40, 221);
+            this.SetMaxiMiniBtn.Name = "SetMaxiMiniBtn";
+            this.SetMaxiMiniBtn.Size = new System.Drawing.Size(200, 23);
+            this.SetMaxiMiniBtn.TabIndex = 6;
+            this.SetMaxiMiniBtn.Text = "MaximizeBox MinimizeBox 속성";
+            this.SetMaxiMiniBtn.UseVisualStyleBackColor = true;
+            this.SetMaxiMiniBtn.Click += new System.EventHandler(this.SetMaxiMiniBtn_Click);
+            // 
+            // instructionTextBox
+            // 
+            this.instructionTextBox.Location = new System.Drawing.Point(40, 13);
+            this.instructionTextBox.Multiline = true;
+            this.instructionTextBox.Name = "instructionTextBox";
+            this.instructionTextBox.Size = new System.Drawing.Size(660, 54);
+            this.instructionTextBox.TabIndex = 7;
             // 
             // Properties
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.instructionTextBox);
+            this.Controls.Add(this.SetMaxiMiniBtn);
             this.Controls.Add(this.SetControlBoxBtn);
             this.Controls.Add(this.ChangeFormBorderStyleBtn);
             this.Controls.Add(this.LoadBitmapBtn);
             this.Controls.Add(this.ReadFromAppConfigBtn);
-            this.Controls.Add(this.SetNotTopMostBtn);
             this.Controls.Add(this.TopMostBtn);
             this.Name = "Properties";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button TopMostBtn;
-        private System.Windows.Forms.Button SetNotTopMostBtn;
         private System.Windows.Forms.Button ReadFromAppConfigBtn;
         private System.Windows.Forms.Button LoadBitmapBtn;
         private System.Windows.Forms.Button ChangeFormBorderStyleBtn;
         private System.Windows.Forms.Button SetControlBoxBtn;
+        private System.Windows.Forms.Button SetMaxiMiniBtn;
+        private System.Windows.Forms.TextBox instructionTextBox;
     }
 }
--- BasicProperties_dll/Properties.cs
+++ BasicProperties_dll/Properties.cs
@@ -21,12 +21,13 @@ namespace BasicProperties_dll
 
         private void TopMostBtn_Click(object sender, EventArgs e)
         {
-            this.TopMost = true;
-        }
-
-        private void SetNotTopMostBtn_Click(object sender, EventArgs e)
-        {
-            this.TopMost = false;
+            this.TopMost = this.TopMost.Equals(true) ? false : true;
+            if (this.TopMost.Equals(true))
+                this.TopMostBtn.BackColor = Color.Red;
+            else
+                this.TopMostBtn.BackColor = mControlColor;
+            this.Text = "this.TopMost = " + this.TopMost.ToString();
+            this.instructionTextBox.Text = "다른 창들보다 항상 위에 있도록 설정";
         }
 
         private void ReadFromAppConfigBtn_Click(object sender, EventArgs e)
@@ -42,6 +43,7 @@ namespace BasicProperties_dll
             {
                 MessageBox.Show(ex.ToString());
                 MessageBox.Show("각 프로젝트의 App.config파일 안에 <configuration>안에 <appSettings> </appSettings>사이에 <add key = \"MainFormOpacity\" value=\".5\" /> 를 추가해 주세요.");
+                instructionTextBox.Text = "프로젝트 폴더에 App.config파일안에 key값으로 접근하여 프로그램에서 적재 가능한데\r\n없을 경우, 예외 메시지 표시하고, 설명 표시";
             }
         }
 
@@ -54,6 +56,7 @@ namespace BasicProperties_dll
                 MessageBox.Show("1. 추가_기존항목으로 추가. 2.파일 오른쪽 클릭_속성_빌드작업_포함 리소스 선택 후, new Bitmap(this.GetType(), \"cocoa.png\" 하면 됩니다. 그러면, 리소스 이름은 Properties.cocoa.png이 된다.");
                 MessageBox.Show("아니면 디자이너에서 [속성]_BackgroundImage옆 ... 을 누른뒤에 선택한다.");
             }
+            instructionTextBox.Text = "C:\\cocoa.png파일이 없으면, 예외 메시지를 표시하고, Bitmap 객체로 적재하는 법 설명";
         }
 
         private void ChangeFormBorderStyleBtn_Click(object sender, EventArgs e)
@@ -73,7 +76,9 @@ namespace BasicProperties_dll
             }
             else
                 ChangeFormBorderStyleBtn.BackColor = Color.Silver;
-            this.Text = this.FormBorderStyle.ToString();
+            this.Text = "this.FormBorderStyle = " + this.FormBorderStyle.ToString();
+            instructionTextBox.Text = "폼에 테두리가 있는지, 크기 조정이 가능한지\r\n보통크기의 캡션 표시줄인지, 작은 크기의 캡션 표시줄인지";
+
         }
 
         private void SetControlBoxBtn_Click(object sender, EventArgs e)
@@ -84,7 +89,20 @@ namespace BasicProperties_dll
             else
                 this.SetControlBoxBtn.BackColor = mControlColor;
 
-            this.Text = this.ControlBox.ToString();
+            this.Text = "this.ControlBox = " + this.ControlBox.ToString();
+            instructionTextBox.Text = "폼의 좌측 상단에 아이콘을 위치시킬지, 우측상단에 종료 버튼을 위치시킬지,\r\n왼쪽상단 왼쪽버튼 클릭시 또는 오른쪽 상단 오른쪽 버튼 클릭시 메뉴 안 뜸";
+        }
+
+        private void SetMaxiMiniBtn_Click(object sender, EventArgs e)
+        {
+            this.MaximizeBox = this.MaximizeBox.Equals(true) ? false : true;
+            this.MinimizeBox = this.MinimizeBox.Equals(true) ? false : true;
+            if (this.MaximizeBox.Equals(false))
+                this.SetMaxiMiniBtn.BackColor = Color.Red;
+            else
+                this.SetMaxiMiniBtn.BackColor = mControlColor;
+            this.Text = "this.MaximizeBox = " + this.MaximizeBox.ToString() + ", this.MinimizeBox = " + this.MinimizeBox.ToString();
+            instructionTextBox.Text = "최소화, 최대화 버튼 표시";
         }
     }
 }
