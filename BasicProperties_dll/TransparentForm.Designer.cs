862f35374840751d2a2b237ed670ca9daf5836af
이기석 <lightandliberty@naver.com>
2021-08-25 19:24:22 +0900

    Mouse로 투명한 창 이동까지 완료

--- BasicProperties_dll/TransparentForm.Designer.cs
+++ BasicProperties_dll/TransparentForm.Designer.cs
@@ -28,22 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CenterBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // CenterBtn
+            // 
+            this.CenterBtn.Location = new System.Drawing.Point(144, 117);
+            this.CenterBtn.Name = "CenterBtn";
+            this.CenterBtn.Size = new System.Drawing.Size(157, 48);
+            this.CenterBtn.TabIndex = 0;
+            this.CenterBtn.Text = "타원 TransparentForm";
+            this.CenterBtn.UseVisualStyleBackColor = true;
             // 
             // TransparentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 190);
+            this.ClientSize = new System.Drawing.Size(414, 285);
+            this.Controls.Add(this.CenterBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TransparentForm";
             this.Text = "TransparentForm";
-            this.TransparencyKey = System.Drawing.SystemColors.Control;
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TransparentForm_Paint);
+            this.Load += new System.EventHandler(this.TransparentForm_Load);
+            this.SizeChanged += new System.EventHandler(this.TransparentForm_SizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TransparentForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TransparentForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TransparentForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TransparentForm_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CenterBtn;
     }
 }
--- BasicProperties_dll/TransparentForm.cs
+++ BasicProperties_dll/TransparentForm.cs
@@ -15,20 +15,74 @@ namespace BasicProperties_dll
         public TransparentForm()
         {
             InitializeComponent();
-            //this.TransparencyKey = System.Drawing.SystemColors.Control;
+            //this.TransparencyKey = System.Drawing.SystemColors.Control; // 디자이너 모드의 속성에서 직접 설정해 주었음
+
         }
 
         // 폼을 다시 그려야 할 때 발생
         private void TransparentForm_Paint(object sender, PaintEventArgs e)
         {
-            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Brown);
-            System.Drawing.Graphics formGraphics;
-            formGraphics = this.CreateGraphics();
-            formGraphics.FillEllipse(myBrush, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
-            // Brush, Graphics 객체는 항상 Dispose()해주는 게 좋음.(MSDN에서 추천)
-//            formGraphics.DrawEllipse(System.Drawing.Pens.Black, new Rectangle(0, 0, 150, 100));
-            myBrush.Dispose();
-            formGraphics.Dispose();
+            //System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Brown);
+            //System.Drawing.Graphics formGraphics;
+            //formGraphics = this.CreateGraphics();
+            //formGraphics.FillEllipse(myBrush, new Rectangle(0, 0, this.ClientSize.Width / 2, this.ClientSize.Height / 2));
+            //formGraphics.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Blue), new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            //// Brush, Graphics 객체는 항상 Dispose()해주는 게 좋음.(MSDN에서 추천)
+            ////            formGraphics.DrawEllipse(System.Drawing.Pens.Black, new Rectangle(0, 0, 150, 100));
+            //myBrush.Dispose();
+            //formGraphics.Dispose();
+        }
+
+        private void TransparentForm_Load(object sender, EventArgs e)
+        {
+            // 폼이 로드될 때 구역(region)을 다시 설정.
+            SetEllipseRegion();
+        }
+
+        private void TransparentForm_SizeChanged(object sender, EventArgs e)
+        {
+            // 폼의 크기가 변경될 때, 구역(region)을 다시 설정.
+            SetEllipseRegion();
+        }
+
+        void SetEllipseRegion()
+        {
+            // FormBorderStyle속성이 None으로 설정되어 있다고 가정한다.
+            Rectangle rect = this.ClientRectangle;
+            using (System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath())
+            {
+                path.AddEllipse(rect);  // 현재 경로에 타원을 추가합니다.
+                this.Region = new Region(path);     // 타원 영역을 구역으로 직접 설정
+            }
+        }
+
+        public Point downPoint = Point.Empty;       // 누른 마우스의 좌표
+
+        private void TransparentForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return; // 왼쪽 버튼을 누른 게 아니면, 종료
+            downPoint = new Point(e.X, e.Y); // 현재 마우스의 X, Y좌표를 downPoint에 설정함.
+
+        }
+
+        private void TransparentForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (downPoint == Point.Empty) return;
+            // 이동한 후의 위치 = 이동한 포인터의 위치 + 폼의 왼쪽 경계선 위치.(이동한 거리 + 폼의 왼쪽 위 좌표)
+            Point location = new Point(this.Left + e.X - downPoint.X, this.Top + e.Y - downPoint.Y);
+            this.Location = location;
+        }
+
+        private void TransparentForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            downPoint = Point.Empty;
+        }
+
+        private void TransparentForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
         }
     }
 }
