da9178b9859238cd2495013b8c2ad6ab9de2ca13
이기석 <lightandliberty@naver.com>
2021-09-02 16:05:01 +0900

    FlatAppearanceForm까지 완료

---
+++ BasicProperties_dll/FlatAppearanceForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace BasicProperties_dll
+{
+    partial class FlatAppearanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SetApplicationEnableVisualStylesBtn = new System.Windows.Forms.Button();
+            this.SetBorderColorBtn = new System.Windows.Forms.Button();
+            this.SetBorderSizeBtn = new System.Windows.Forms.Button();
+            this.SetMouseDownBackColorBtn = new System.Windows.Forms.Button();
+            this.SetMouseOverBackColorBtn = new System.Windows.Forms.Button();
+            this.FlatBtn = new System.Windows.Forms.Button();
+            this.PopupBtn = new System.Windows.Forms.Button();
+            this.StandardBtn = new System.Windows.Forms.Button();
+            this.SystemBtn = new System.Windows.Forms.Button();
+            this.SetFlatAppearanceLabel = new System.Windows.Forms.Label();
+            this.SetFlatStyleLabel = new System.Windows.Forms.Label();
+            this.BorderSizeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.BorderSizeNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SetApplicationEnableVisualStylesBtn
+            // 
+            this.SetApplicationEnableVisualStylesBtn.Location = new System.Drawing.Point(189, 47);
+            this.SetApplicationEnableVisualStylesBtn.Name = "SetApplicationEnableVisualStylesBtn";
+            this.SetApplicationEnableVisualStylesBtn.Size = new System.Drawing.Size(232, 77);
+            this.SetApplicationEnableVisualStylesBtn.TabIndex = 0;
+            this.SetApplicationEnableVisualStylesBtn.Text = "Application.EnableVisualStyles()";
+            this.SetApplicationEnableVisualStylesBtn.UseVisualStyleBackColor = true;
+            this.SetApplicationEnableVisualStylesBtn.Click += new System.EventHandler(this.SetApplicationEnableVisualStylesBtn_Click);
+            // 
+            // SetBorderColorBtn
+            // 
+            this.SetBorderColorBtn.Location = new System.Drawing.Point(57, 185);
+            this.SetBorderColorBtn.Name = "SetBorderColorBtn";
+            this.SetBorderColorBtn.Size = new System.Drawing.Size(232, 23);
+            this.SetBorderColorBtn.TabIndex = 1;
+            this.SetBorderColorBtn.Text = "SetBorderColor";
+            this.SetBorderColorBtn.UseVisualStyleBackColor = true;
+            this.SetBorderColorBtn.Click += new System.EventHandler(this.SetBorderColorBtn_Click);
+            // 
+            // SetBorderSizeBtn
+            // 
+            this.SetBorderSizeBtn.Location = new System.Drawing.Point(57, 214);
+            this.SetBorderSizeBtn.Name = "SetBorderSizeBtn";
+            this.SetBorderSizeBtn.Size = new System.Drawing.Size(169, 23);
+            this.SetBorderSizeBtn.TabIndex = 2;
+            this.SetBorderSizeBtn.Text = "SetBorderSize (pixel unit)";
+            this.SetBorderSizeBtn.UseVisualStyleBackColor = true;
+            // 
+            // SetMouseDownBackColorBtn
+            // 
+            this.SetMouseDownBackColorBtn.Location = new System.Drawing.Point(57, 243);
+            this.SetMouseDownBackColorBtn.Name = "SetMouseDownBackColorBtn";
+            this.SetMouseDownBackColorBtn.Size = new System.Drawing.Size(232, 23);
+            this.SetMouseDownBackColorBtn.TabIndex = 3;
+            this.SetMouseDownBackColorBtn.Text = "SetMouseDownBackColor";
+            this.SetMouseDownBackColorBtn.UseVisualStyleBackColor = true;
+            this.SetMouseDownBackColorBtn.Click += new System.EventHandler(this.SetMouseDownBackColorBtn_Click);
+            // 
+            // SetMouseOverBackColorBtn
+            // 
+            this.SetMouseOverBackColorBtn.Location = new System.Drawing.Point(57, 272);
+            this.SetMouseOverBackColorBtn.Name = "SetMouseOverBackColorBtn";
+            this.SetMouseOverBackColorBtn.Size = new System.Drawing.Size(232, 23);
+            this.SetMouseOverBackColorBtn.TabIndex = 4;
+            this.SetMouseOverBackColorBtn.Text = "SetMouseOverBackColor";
+            this.SetMouseOverBackColorBtn.UseVisualStyleBackColor = true;
+            this.SetMouseOverBackColorBtn.Click += new System.EventHandler(this.SetMouseOverBackColorBtn_Click);
+            // 
+            // FlatBtn
+            // 
+            this.FlatBtn.Location = new System.Drawing.Point(314, 185);
+            this.FlatBtn.Name = "FlatBtn";
+            this.FlatBtn.Size = new System.Drawing.Size(232, 23);
+            this.FlatBtn.TabIndex = 5;
+            this.FlatBtn.Text = "Flat";
+            this.FlatBtn.UseVisualStyleBackColor = true;
+            this.FlatBtn.Click += new System.EventHandler(this.FlatBtn_Click);
+            // 
+            // PopupBtn
+            // 
+            this.PopupBtn.Location = new System.Drawing.Point(314, 214);
+            this.PopupBtn.Name = "PopupBtn";
+            this.PopupBtn.Size = new System.Drawing.Size(232, 23);
+            this.PopupBtn.TabIndex = 6;
+            this.PopupBtn.Text = "Popup";
+            this.PopupBtn.UseVisualStyleBackColor = true;
+            this.PopupBtn.Click += new System.EventHandler(this.PopupBtn_Click);
+            // 
+            // StandardBtn
+            // 
+            this.StandardBtn.Location = new System.Drawing.Point(314, 243);
+            this.StandardBtn.Name = "StandardBtn";
+            this.StandardBtn.Size = new System.Drawing.Size(232, 23);
+            this.StandardBtn.TabIndex = 7;
+            this.StandardBtn.Text = "Standard";
+            this.StandardBtn.UseVisualStyleBackColor = true;
+            this.StandardBtn.Click += new System.EventHandler(this.StandardBtn_Click);
+            // 
+            // SystemBtn
+            // 
+            this.SystemBtn.Location = new System.Drawing.Point(314, 272);
+            this.SystemBtn.Name = "SystemBtn";
+            this.SystemBtn.Size = new System.Drawing.Size(232, 23);
+            this.SystemBtn.TabIndex = 8;
+            this.SystemBtn.Text = "System";
+            this.SystemBtn.UseVisualStyleBackColor = true;
+            this.SystemBtn.Click += new System.EventHandler(this.SystemBtn_Click);
+            // 
+            // SetFlatAppearanceLabel
+            // 
+            this.SetFlatAppearanceLabel.AutoSize = true;
+            this.SetFlatAppearanceLabel.Location = new System.Drawing.Point(115, 166);
+            this.SetFlatAppearanceLabel.Name = "SetFlatAppearanceLabel";
+            this.SetFlatAppearanceLabel.Size = new System.Drawing.Size(111, 12);
+            this.SetFlatAppearanceLabel.TabIndex = 9;
+            this.SetFlatAppearanceLabel.Text = "SetFlatAppearance";
+            // 
+            // SetFlatStyleLabel
+            // 
+            this.SetFlatStyleLabel.AutoSize = true;
+            this.SetFlatStyleLabel.Location = new System.Drawing.Point(392, 166);
+            this.SetFlatStyleLabel.Name = "SetFlatStyleLabel";
+            this.SetFlatStyleLabel.Size = new System.Drawing.Size(71, 12);
+            this.SetFlatStyleLabel.TabIndex = 10;
+            this.SetFlatStyleLabel.Text = "SetFlatStyle";
+            // 
+            // BorderSizeNumericUpDown
+            // 
+            this.BorderSizeNumericUpDown.Location = new System.Drawing.Point(233, 215);
+            this.BorderSizeNumericUpDown.Name = "BorderSizeNumericUpDown";
+            this.BorderSizeNumericUpDown.Size = new System.Drawing.Size(56, 21);
+            this.BorderSizeNumericUpDown.TabIndex = 11;
+            this.BorderSizeNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.BorderSizeNumericUpDown.ValueChanged += new System.EventHandler(this.BorderSizeNumericUpDown_ValueChanged);
+            this.BorderSizeNumericUpDown.Enter += new System.EventHandler(this.BorderSizeNumericUpDown_Enter);
+            // 
+            // FlatAppearanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BorderSizeNumericUpDown);
+            this.Controls.Add(this.SetFlatStyleLabel);
+            this.Controls.Add(this.SetFlatAppearanceLabel);
+            this.Controls.Add(this.SystemBtn);
+            this.Controls.Add(this.StandardBtn);
+            this.Controls.Add(this.PopupBtn);
+            this.Controls.Add(this.FlatBtn);
+            this.Controls.Add(this.SetMouseOverBackColorBtn);
+            this.Controls.Add(this.SetMouseDownBackColorBtn);
+            this.Controls.Add(this.SetBorderSizeBtn);
+            this.Controls.Add(this.SetBorderColorBtn);
+            this.Controls.Add(this.SetApplicationEnableVisualStylesBtn);
+            this.Name = "FlatAppearanceForm";
+            this.Text = "FlatAppearanceForm";
+            this.Load += new System.EventHandler(this.FlatAppearanceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BorderSizeNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SetApplicationEnableVisualStylesBtn;
+        private System.Windows.Forms.Button SetBorderColorBtn;
+        private System.Windows.Forms.Button SetBorderSizeBtn;
+        private System.Windows.Forms.Button SetMouseDownBackColorBtn;
+        private System.Windows.Forms.Button SetMouseOverBackColorBtn;
+        private System.Windows.Forms.Button FlatBtn;
+        private System.Windows.Forms.Button PopupBtn;
+        private System.Windows.Forms.Button StandardBtn;
+        private System.Windows.Forms.Button SystemBtn;
+        private System.Windows.Forms.Label SetFlatAppearanceLabel;
+        private System.Windows.Forms.Label SetFlatStyleLabel;
+        private System.Windows.Forms.NumericUpDown BorderSizeNumericUpDown;
+    }
+}
---
+++ BasicProperties_dll/FlatAppearanceForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicProperties_dll
+{
+    public partial class FlatAppearanceForm : Form
+    {
+        public FlatAppearanceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SetApplicationEnableVisualStylesBtn_Click(object sender, EventArgs e)
+        {
+            switch(Application.VisualStyleState)
+            {
+                case System.Windows.Forms.VisualStyles.VisualStyleState.ClientAndNonClientAreasEnabled:
+                    Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.ClientAreaEnabled;
+                    this.SetApplicationEnableVisualStylesBtn.Text = System.Windows.Forms.VisualStyles.VisualStyleState.ClientAreaEnabled.ToString();
+                    break;
+                case System.Windows.Forms.VisualStyles.VisualStyleState.ClientAreaEnabled :
+                    Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.NonClientAreaEnabled;
+                    this.SetApplicationEnableVisualStylesBtn.Text = System.Windows.Forms.VisualStyles.VisualStyleState.NonClientAreaEnabled.ToString();
+                    break;
+                case System.Windows.Forms.VisualStyles.VisualStyleState.NonClientAreaEnabled :
+                    Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.NoneEnabled;
+                    this.SetApplicationEnableVisualStylesBtn.Text = System.Windows.Forms.VisualStyles.VisualStyleState.NoneEnabled.ToString();
+                    break;
+                case System.Windows.Forms.VisualStyles.VisualStyleState.NoneEnabled:
+                    Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.ClientAndNonClientAreasEnabled;
+                    this.SetApplicationEnableVisualStylesBtn.Text = System.Windows.Forms.VisualStyles.VisualStyleState.ClientAndNonClientAreasEnabled.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void FlatAppearanceForm_Load(object sender, EventArgs e)
+        {
+            SetApplicationEnableVisualStylesBtn.Text = Application.VisualStyleState.ToString();
+        }
+
+        private void FlatBtn_Click(object sender, EventArgs e)
+        {
+            this.FlatBtn.FlatStyle = FlatStyle.Flat;
+            this.SetApplicationEnableVisualStylesBtn.FlatStyle = FlatStyle.Flat;
+        }
+
+        private void PopupBtn_Click(object sender, EventArgs e)
+        {
+            this.PopupBtn.FlatStyle = FlatStyle.Popup;
+            this.SetApplicationEnableVisualStylesBtn.FlatStyle = FlatStyle.Popup;
+        }
+
+        private void StandardBtn_Click(object sender, EventArgs e)
+        {
+            this.StandardBtn.FlatStyle = FlatStyle.Standard;
+            this.SetApplicationEnableVisualStylesBtn.FlatStyle = FlatStyle.Standard;
+        }
+
+        private void SystemBtn_Click(object sender, EventArgs e)
+        {
+            this.StandardBtn.FlatStyle = FlatStyle.System;
+            this.SetApplicationEnableVisualStylesBtn.FlatStyle = FlatStyle.System;
+        }
+
+        private void BorderSizeNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            this.SetApplicationEnableVisualStylesBtn.FlatAppearance.BorderSize = (int)this.BorderSizeNumericUpDown.Value;
+            this.SetBorderSizeBtn.FlatStyle = FlatStyle.Flat;
+            this.SetBorderSizeBtn.FlatAppearance.BorderSize = (int)this.BorderSizeNumericUpDown.Value;
+            this.SetBorderSizeBtn.Text = FlatStyle.Flat.ToString() + " BorderSize = " + this.SetApplicationEnableVisualStylesBtn.FlatAppearance.BorderSize.ToString();
+
+            this.SetApplicationEnableVisualStylesBtn.Text = this.SetApplicationEnableVisualStylesBtn.FlatAppearance.BorderSize.ToString();
+
+        }
+
+        private void BorderSizeNumericUpDown_Enter(object sender, EventArgs e)
+        {
+            this.BorderSizeNumericUpDown.Select(0, (int)(this.BorderSizeNumericUpDown.Value / 10) + 1);
+        }
+
+        private void SetBorderColorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.FullOpen = true;
+            if (DialogResult.OK == colorDialog.ShowDialog())
+            {
+                this.SetBorderColorBtn.FlatAppearance.BorderColor = colorDialog.Color;
+                this.SetApplicationEnableVisualStylesBtn.FlatAppearance.BorderColor = colorDialog.Color;
+            }
+            colorDialog.Dispose();
+        }
+
+        private void SetMouseDownBackColorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.FullOpen = true;
+            if(DialogResult.OK == colorDialog.ShowDialog())
+            {
+                this.SetMouseDownBackColorBtn.FlatAppearance.MouseDownBackColor = colorDialog.Color;
+                this.SetApplicationEnableVisualStylesBtn.FlatAppearance.MouseDownBackColor = colorDialog.Color;
+            }
+            colorDialog.Dispose();
+        }
+
+        private void SetMouseOverBackColorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.FullOpen = true;
+            if (DialogResult.OK == colorDialog.ShowDialog())
+            {
+                this.SetMouseOverBackColorBtn.FlatAppearance.MouseOverBackColor = colorDialog.Color;
+                this.SetApplicationEnableVisualStylesBtn.FlatAppearance.MouseOverBackColor = colorDialog.Color;
+            }
+            colorDialog.Dispose();
+        }
+    }
+}
